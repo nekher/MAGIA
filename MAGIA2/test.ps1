af6788366af952bf1791a1a9f047693ec810eff5
nekher <max.mym@gmail.com>
2025-02-17 21:29:20 -0300

    casicasi

--- MAGIA2/test.ps1
+++ MAGIA2/test.ps1
@@ -47,8 +47,24 @@ function Optimizar-PC {
     Start-Process "$env:windir\system32\SystemPropertiesPerformance.exe"
     Start-Process "msconfig"
     
-    [System.Windows.Forms.MessageBox]::Show("Optimización al mango. Decime...no merezco un cafecito?")
+    [System.Windows.Forms.MessageBox]::Show("Optimizacion al mango. Decime...no merezco un cafecito?")
 }
+
+# Función para Poner los updates en ON
+function Updates-On {
+        
+    [System.Windows.Forms.MessageBox]::Show("Wiii!!! Volvieron los Updates!!! (?) ")
+}
+
+
+
+# Función para Poner los updates en OFF
+function Updates-Off {
+        
+    [System.Windows.Forms.MessageBox]::Show("Y asi, como si nada, se fueron los updates...que dia triste :(")
+}
+
+
 
 # Función para mostrar las explicaciones
 function Mostrar-Explicaciones {
@@ -72,11 +88,11 @@ function Mostrar-Explicaciones {
 
     $imageForm = New-Object System.Windows.Forms.Form
     $imageForm.Text = "Explicacion sobre lo que hay que hacer"
-    $imageForm.Size = New-Object System.Drawing.Size(600,1280)
+    $imageForm.Size = New-Object System.Drawing.Size(720,1280)
 
     $pictureBox = New-Object System.Windows.Forms.PictureBox
     $pictureBox.Image = [System.Drawing.Image]::FromFile("c:\repos\magia\magia2\pdf.jpg")
-    $pictureBox.SizeMode = "StretchImage"
+    $pictureBox.SizeMode = "Zoom"
     $pictureBox.Dock = "Fill"
     $imageForm.Controls.Add($pictureBox)
 
@@ -89,23 +105,24 @@ function Mostrar-Explicaciones {
 function Confirmar-Accion {
     # Crear nueva ventana para la confirmación
     $formConfirmacion = New-Object System.Windows.Forms.Form
-    $formConfirmacion.Text = "Confirmación de Acción"
+    $formConfirmacion.Text = "Confirmacion de Accion"
     $formConfirmacion.Size = New-Object System.Drawing.Size(500, 300)
     $formConfirmacion.StartPosition = 'CenterScreen'
 
     # Etiqueta de confirmación
     $labelConfirmacion = New-Object System.Windows.Forms.Label
-    $labelConfirmacion.Text = "¿ESTÁS SEGURO DE LO QUE VAS A HACER? Vas a modificar lo siguiente:`r`n10 campos a editar"
+    $labelConfirmacion.Text = "ESTAS SEGURO?"
     $labelConfirmacion.Size = New-Object System.Drawing.Size(400, 50)
     $labelConfirmacion.Location = New-Object System.Drawing.Point(50, 50)
     $formConfirmacion.Controls.Add($labelConfirmacion)
 
     # Botón NO
     $btnNo = New-Object System.Windows.Forms.Button
-    $btnNo.Text = "NO, me dio miedo y me arrepentí"
+    $btnNo.Text = "NO, TENGO MIEDO"
+    $btnNo.Font = New-Object System.Drawing.Font("Roboto", 13) 
     $btnNo.Size = New-Object System.Drawing.Size(200, 50)
     $btnNo.Location = New-Object System.Drawing.Point(50, 120)
-    $btnNo.BackColor = [System.Drawing.Color]::LightPink
+    $btnNo.BackColor = [System.Drawing.Color]::LightGreen
     $btnNo.Add_Click({
         $btnOptimizar.Visible = $true
         $btnExplicaciones.Visible = $true
@@ -116,10 +133,11 @@ function Confirmar-Accion {
 
     # Botón SI
     $btnSi = New-Object System.Windows.Forms.Button
-    $btnSi.Text = "SI, le voy a dar murra"
+    $btnSi.Text = "SI, DALE MURRA"
+    $btnSi.Font = New-Object System.Drawing.Font("Roboto", 13) 
     $btnSi.Size = New-Object System.Drawing.Size(200, 50)
     $btnSi.Location = New-Object System.Drawing.Point(250, 120)
-    $btnSi.BackColor = [System.Drawing.Color]::LightGreen
+    $btnSi.BackColor = [System.Drawing.Color]::LightPink
     $btnSi.Add_Click({
         Optimizar-PC
         $mensajeOpt = New-Object System.Windows.Forms.Label
@@ -128,7 +146,7 @@ function Confirmar-Accion {
         $mensajeOpt.Location = New-Object System.Drawing.Point(150, 150)
         $form.Controls.Add($mensajeOpt)
 
-        Start-Sleep -Seconds 2
+        Start-Sleep -Seconds 1
         $btnOptimizar.Visible = $true
         $btnExplicaciones.Visible = $true
         $btnSalir.Visible = $true
@@ -140,31 +158,109 @@ function Confirmar-Accion {
     $formConfirmacion.ShowDialog()
 }
 
+# Función para confirmar antes de ejecutar la optimización
+function Updates-OnOff {
+    # Crear nueva ventana para la confirmación
+    $formConfirmacion = New-Object System.Windows.Forms.Form
+    $formConfirmacion.Text = "Elegi que queres hacer"
+    $formConfirmacion.Size = New-Object System.Drawing.Size(500, 300)
+    $formConfirmacion.StartPosition = 'CenterScreen'
+
+    # Etiqueta de confirmación
+    $labelConfirmacion = New-Object System.Windows.Forms.Label
+    $labelConfirmacion.Text = "Choose your poison..."
+    $labelConfirmacion.Size = New-Object System.Drawing.Size(400, 50)
+    $labelConfirmacion.Location = New-Object System.Drawing.Point(50, 50)
+    $formConfirmacion.Controls.Add($labelConfirmacion)
+
+    # Botón Activar Updates
+    $btnOn = New-Object System.Windows.Forms.Button
+    $btnOn.Text = "ACTIVAR UPDATES"
+    $btnOn.Font = New-Object System.Drawing.Font("Roboto", 13) 
+    $btnOn.Size = New-Object System.Drawing.Size(200, 50)
+    $btnOn.Location = New-Object System.Drawing.Point(50, 120)
+    $btnOn.BackColor = [System.Drawing.Color]::LightGreen
+    $btnOn.Add_Click({
+        Updates-On
+        $mensajeOpt = New-Object System.Windows.Forms.Label
+        $mensajeOpt.Text = "Y asi, como si nada, ya no hay updates :( "
+        $mensajeOpt.Size = New-Object System.Drawing.Size(300, 50)
+        $mensajeOpt.Location = New-Object System.Drawing.Point(150, 150)
+        $form.Controls.Add($mensajeOpt)
+
+        $btnOptimizar.Visible = $true
+        $btnExplicaciones.Visible = $true
+        $btnSalir.Visible = $true
+        $formConfirmacion.Close()  # Cerrar ventana de confirmación
+    })
+    $formConfirmacion.Controls.Add($btnOn)
+
+    # Botón Desactivar Updates
+    $btnOff = New-Object System.Windows.Forms.Button
+    $btnOff.Text = "DESACTIVAR UPDATES"
+    $btnOff.Font = New-Object System.Drawing.Font("Roboto", 13) 
+    $btnOff.Size = New-Object System.Drawing.Size(200, 50)
+    $btnOff.Location = New-Object System.Drawing.Point(250, 120)
+    $btnOff.BackColor = [System.Drawing.Color]::LightPink
+    $btnOff.Add_Click({
+        Updates-Off
+        $mensajeOpt = New-Object System.Windows.Forms.Label
+        $mensajeOpt.Text = "Y asi, como si nada, ya no hay updates :( "
+        $mensajeOpt.Size = New-Object System.Drawing.Size(300, 50)
+        $mensajeOpt.Location = New-Object System.Drawing.Point(150, 150)
+        $form.Controls.Add($mensajeOpt)
+
+        $btnOptimizar.Visible = $true
+        $btnExplicaciones.Visible = $true
+        $btnSalir.Visible = $true
+        $formConfirmacion.Close()  # Cerrar ventana de confirmación
+    })
+    $formConfirmacion.Controls.Add($btnOff)
+
+    # Mostrar la ventana de confirmación
+    $formConfirmacion.ShowDialog()
+}
+
+
 # Botones principales con colores diferentes
 
 $btnExplicaciones = New-Object System.Windows.Forms.Button
-$btnExplicaciones.Text = "Explicaciones"
+$btnExplicaciones.Text = "HELP"
+$btnExplicaciones.Font = New-Object System.Drawing.Font("Roboto", 14) 
 $btnExplicaciones.FlatStyle = 'Flat'
 $btnExplicaciones.Size = New-Object System.Drawing.Size(200, 50)
-$btnExplicaciones.Location = New-Object System.Drawing.Point(380, 50)
+$btnExplicaciones.Location = New-Object System.Drawing.Point(360, 30)
 $btnExplicaciones.BackColor = [System.Drawing.Color]::Gold
 $btnExplicaciones.Add_Click({ Mostrar-Explicaciones })
 $form.Controls.Add($btnExplicaciones)
 
 $btnOptimizar = New-Object System.Windows.Forms.Button
-$btnOptimizar.Text = "Hacer que la PC funcione bien"
+$btnOptimizar.Text = "MAGIA"
+$btnOptimizar.Font = New-Object System.Drawing.Font("Roboto", 14) 
 $btnOptimizar.FlatStyle = 'Flat'
 $btnOptimizar.Size = New-Object System.Drawing.Size(200, 50)
-$btnOptimizar.Location = New-Object System.Drawing.Point(380, 120)
+$btnOptimizar.Location = New-Object System.Drawing.Point(360, 100)
 $btnOptimizar.BackColor = [System.Drawing.Color]::Orange
 $btnOptimizar.Add_Click({ Confirmar-Accion })
 $form.Controls.Add($btnOptimizar)
 
+$btnUpdates = New-Object System.Windows.Forms.Button
+$btnUpdates.Text = "UPDATES ON/OFF"
+$btnUpdates.Font = New-Object System.Drawing.Font("Roboto", 14) 
+$btnUpdates.FlatStyle = 'Flat'
+$btnUpdates.Size = New-Object System.Drawing.Size(200, 50)
+$btnUpdates.Location = New-Object System.Drawing.Point(360, 170)
+$btnUpdates.BackColor = [System.Drawing.Color]::Orange
+$btnUpdates.Add_Click({ Updates-OnOff })
+$form.Controls.Add($btnUpdates)
+
+
 $btnSalir = New-Object System.Windows.Forms.Button
 $btnSalir.Text = "Salir"
+$btnSalir.Font = New-Object System.Drawing.Font("Roboto", 14) 
 $btnSalir.Size = New-Object System.Drawing.Size(200, 50)
 $btnSalir.FlatStyle = 'Flat'
-$btnSalir.Location = New-Object System.Drawing.Point(380, 190)
+$btnSalir.Location = New-Object System.Drawing.Point(360, 240)
 $btnSalir.BackColor = [System.Drawing.Color]::Firebrick
 $btnSalir.Add_Click({ $form.Close() })
 $form.Controls.Add($btnSalir)
@@ -173,9 +269,12 @@ $form.Controls.Add($btnSalir)
 # Agregar los botones después del fondo para que queden encima
 $form.Controls.Add($btnExplicaciones)
 $form.Controls.Add($btnOptimizar)
+$form.Controls.Add($btnUpdates)
 $form.Controls.Add($btnSalir)
 $form.Controls.Add($btnNo)
 $form.Controls.Add($btnSi)
+$form.Controls.Add($btnOff)
+$form.Controls.Add($btnOn)
 
 
 # Agregar un PictureBox para mostrar la imagen de fondo
